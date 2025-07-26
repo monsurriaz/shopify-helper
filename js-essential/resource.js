8ca8666d7d3d2bba936fa65f4a3a16857616246a
monsurriaz <monsurrahmanriaz@gmail.com>
2025-07-26 18:29:55 +0600

    recource.js updated

--- js-essential/resource.js
+++ js-essential/resource.js
@@ -1,2 +1,8 @@
 // get int value from string
 let floatValue = parseFloat(string.replace(/[^0-9.]/g, ''));
+
+// set section's height or width as global css variable. this type of small code can be used inside each section.
+document.documentElement.style.setProperty(
+  '--announcement-bar-height', 
+  `${document.getElementById('shopify-section-{{ section.id }}').clientHeight.toFixed(2)}px`
+);
